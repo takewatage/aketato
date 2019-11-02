2c94f4d0943e2e5b0e038c68a9c378b4c60139e9
ishizuka takeaki <>
2019-11-02 23:00:31 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        aketato
+      </h1>
+      <h2 class="subtitle">
+        My cat&#39;s meow Nuxt.js project
+      </h2>
+      <v-btn small>Normal</v-btn>
+      <v-btn small>Normal</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss">
+  .title {
+    font-size: 100px;
+  }
+</style>
